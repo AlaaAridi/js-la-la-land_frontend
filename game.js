8c4d3961e27ec52c6fdc4c2856444f03d71f2177
AlaaAridi <112479275+AlaaAridi@users.noreply.github.com>
2022-09-05 20:08:51 +0300

    loading issue solved

--- game.js
+++ game.js
@@ -1,48 +1,32 @@
 //checking if the document is loaded
-/*
-if (document.readyState === "complete") {
-      console.log("document.readyState: ", document.readyState, 
-        `- The page DOM with Sub-resources are now fully loaded. `,
-        `- "load" event`
-        )
-}
 
-document.addEventListener('DOMContentLoaded', function(event) {
-  
-})
-*/
-document.onload=funct1()
+window.addEventListener("load", function(event) {
 
+            // creating constants
 
-// creating constants
+            const status = document.getElementById("status")
+            const game = document.getElementById("game")
+            const start = document.getElementById("start")
+            const boundary1 = document.getElementById("boundary1")
+            const end = document.getElementById("end")
 
-const status = document.getElementById("status")
-const game = document.getElementById("game")
-const start = document.getElementById("start")
-const boundary1 = document.getElementById("boundary1")
-const end = document.getElementById("end")
+            const boundaries = document.getElementsByClassName("boundary")
+            const example = document.getElementsByClassName("example")
 
-const boundaries = document.getElementsByClassName("boundary")
-const example = document.getElementsByClassName("example")
+            //creating functions
 
-//creating functions
+            function funct1() {
+                console.log("hello world")
+                boundaries[1].style.color = "brown"
+            }
 
-function funct1() {
-    console.log("hello world")
-    /*boundaries[1].style.color="red"*/
-}
+            function funct2() {
+                console.log("heloo world")
+            }
 
-function funct2(){
-  console.log("heloo world")
-}
-
-funct2()
-
-console.info(document.readyState)
-
-console.log("hi")
-
-/*for(let i=0;i<boundaries.length;i++){
-    boundaries(i).style.color="yellow"
-}
-*/
+            function hit() {
+                for (let i = 0; i < boundaries.length; i++) {
+                    boundaries[i].style.backgroundColor = "red"
+                }
+            }
+        }
